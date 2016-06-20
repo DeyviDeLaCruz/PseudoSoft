e6baf178a04a9caa3501f3d113163221ab5c95d3
Deyvi De La Cruz <dekhan06@gmail.com>
2016-06-20 16:40:04 -0500

    Estructura Inicial

---
+++ PseudoSoft/src/anocongruenciales/Cuadradosmedios.java
@@ -0,0 +1,74 @@
+
+package anocongruenciales;
+
+public class Cuadradosmedios {
+    
+    //Variables propias de la clase
+    private String teoriacm="";
+    private String instrucciones="";
+    
+    private int semilla;
+    private int semillatemp;
+    
+    //constructor
+
+    public Cuadradosmedios() {
+    }
+
+    public Cuadradosmedios(int semilla, int semillatemp) {
+        this.semilla = semilla;
+        this.semillatemp = semillatemp;
+    }
+    
+
+    //get y set
+
+    public String getTeoriacm() {
+        return teoriacm;
+    }
+
+    public void setTeoriacm(String teoriacm) {
+        this.teoriacm = teoriacm;
+    }
+
+    public String getInstrucciones() {
+        return instrucciones;
+    }
+
+    public void setInstrucciones(String instrucciones) {
+        this.instrucciones = instrucciones;
+    }
+
+    public int getSemilla() {
+        return semilla;
+    }
+
+    public void setSemilla(int semilla) {
+        this.semilla = semilla;
+    }
+
+    public int getSemillatemp() {
+        return semillatemp;
+    }
+
+    public void setSemillatemp(int semillatemp) {
+        this.semillatemp = semillatemp;
+    }
+    
+    
+    
+    //funciones
+    public void Generarcm(int semilla){
+    
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
