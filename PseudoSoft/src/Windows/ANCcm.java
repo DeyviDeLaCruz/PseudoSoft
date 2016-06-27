d968a5a33f3886ded9f7aa276f0c55325be8447b
Deyvi De La Cruz <dekhan06@gmail.com>
2016-06-27 18:20:36 -0500

    Agregado ANCcm al paquete Windows

--- PseudoSoft/src/Windows/ANCcm.java
+++ PseudoSoft/src/Windows/ANCcm.java
@@ -28,39 +28,79 @@ public class ANCcm extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        titulo1 = new javax.swing.JLabel();
+        titulo2 = new javax.swing.JLabel();
+        lbl_semilla = new javax.swing.JLabel();
+        lbl_cantidad = new javax.swing.JLabel();
+        txt_semilla = new javax.swing.JTextField();
+        txt_cantidad = new javax.swing.JTextField();
+        btn_generar = new javax.swing.JButton();
+        lst_semilla2 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList();
+        lst_resultado = new javax.swing.JScrollPane();
+        jList2 = new javax.swing.JList();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setText("jLabel1");
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 593, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 195, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 404, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 122, Short.MAX_VALUE))
-        );
+        titulo1.setFont(new java.awt.Font("Adobe Garamond Pro Bold", 1, 24)); // NOI18N
+        titulo1.setForeground(new java.awt.Color(255, 255, 255));
+        titulo1.setText("ALGORITMO DE ");
+        jPanel1.add(titulo1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 30, 200, 60));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        titulo2.setFont(new java.awt.Font("Adobe Garamond Pro", 1, 24)); // NOI18N
+        titulo2.setForeground(new java.awt.Color(226, 103, 13));
+        titulo2.setText("CUADRADOS MEDIOS");
+        jPanel1.add(titulo2, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 25, -1, 70));
+
+        lbl_semilla.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        lbl_semilla.setForeground(new java.awt.Color(255, 255, 255));
+        lbl_semilla.setText("SEMILLA:");
+        jPanel1.add(lbl_semilla, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 120, -1, -1));
+
+        lbl_cantidad.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        lbl_cantidad.setForeground(new java.awt.Color(255, 255, 255));
+        lbl_cantidad.setText("CANTIDAD A GENERAR");
+        jPanel1.add(lbl_cantidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 120, -1, -1));
+
+        txt_semilla.setFont(new java.awt.Font("Adobe Garamond Pro", 1, 18)); // NOI18N
+        jPanel1.add(txt_semilla, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 140, 190, 50));
+
+        txt_cantidad.setFont(new java.awt.Font("Adobe Garamond Pro", 1, 18)); // NOI18N
+        jPanel1.add(txt_cantidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 140, 120, 50));
+
+        btn_generar.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        btn_generar.setForeground(new java.awt.Color(226, 103, 13));
+        btn_generar.setText("GENERAR");
+        btn_generar.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btn_generar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(btn_generar, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 140, 120, 50));
+
+        jList1.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        lst_semilla2.setViewportView(jList1);
+
+        jPanel1.add(lst_semilla2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 260, 190, 220));
+
+        jList2.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        lst_resultado.setViewportView(jList2);
+
+        jPanel1.add(lst_resultado, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 260, 170, 220));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/ANCcm_fondo.png"))); // NOI18N
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 833, 509));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -101,7 +141,18 @@ public class ANCcm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_generar;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JList jList1;
+    private javax.swing.JList jList2;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lbl_cantidad;
+    private javax.swing.JLabel lbl_semilla;
+    private javax.swing.JScrollPane lst_resultado;
+    private javax.swing.JScrollPane lst_semilla2;
+    private javax.swing.JLabel titulo1;
+    private javax.swing.JLabel titulo2;
+    private javax.swing.JTextField txt_cantidad;
+    private javax.swing.JTextField txt_semilla;
     // End of variables declaration//GEN-END:variables
 }
